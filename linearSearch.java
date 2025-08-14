8d32ca0d17b86b8e44fac58b497f38f69cf7af19
Anshu4o4 <anshu4o4error@gmaill.com>
2025-08-14 22:47:22 +0530

    8th

---
+++ linearSearch.java
@@ -0,0 +1,23 @@
+public class linearSearch {
+
+    public static int linearS(int numbers[],int key){
+        for(int i=0;i<=numbers.length;i++){
+            if(numbers[i]==key){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int numbers[]={1,2,3,4,5,6,7,8,9};
+        int key=7;
+        int index=linearS(numbers, key);
+
+        if(index==-1){
+            System.out.println("item not found");
+        }else{
+            System.out.println("key is at index: "+ index);
+        }
+    }
+    
+}
